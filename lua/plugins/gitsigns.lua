7737606c9feee640590cca6256ecae93e44ef416
LICF01 <lucascubilla@gmail.com>
2023-09-11 10:07:34 -0400

    mod mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,67 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		local gitsigns = require("gitsigns")
+
+		gitsigns.setup({
+
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]h", function()
+					if vim.wo.diff then
+						return "]h"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "next hunk" })
+
+				map("n", "[h", function()
+					if vim.wo.diff then
+						return "[h"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "previous hunk" })
+
+				-- Actions
+				map("n", "<leader>ghs", gs.stage_hunk, { desc = "stage" })
+				map("n", "<leader>ghr", gs.reset_hunk, { desc = "reset" })
+				map("v", "<leader>ghs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "stage" })
+				map("v", "<leader>hr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "reset" })
+				map("n", "<leader>ghS", gs.stage_buffer, { desc = "stage buffer" })
+				map("n", "<leader>ghu", gs.undo_stage_hunk, { desc = "undo stage" })
+				map("n", "<leader>ghR", gs.reset_buffer, { desc = "reset buffer" })
+				map("n", "<leader>ghp", gs.preview_hunk, { desc = "preview" })
+				map("n", "<leader>ghb", function()
+					gs.blame_line({ full = true })
+				end, { desc = "blame line" })
+				map("n", "<leader>ghd", gs.diffthis, { desc = "diff" })
+				map("n", "<leader>ghD", function()
+					gs.diffthis("~")
+				end)
+				map("n", "<leader>ugd", gs.toggle_deleted, { desc = "toggle deleted" })
+				map("n", "<leader>ugb", gs.toggle_current_line_blame, { desc = "toggle line blame" })
+
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+			end,
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,11 +31,24 @@ return {
 		telescope.load_extension("fzf")
 
 		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+		local keymap = vim.keymap
+		local builtin = require("telescope.builtin")
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Files in cwd" })
+		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+		keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "String in buffer" })
+		keymap.set("n", "<leader>fS", builtin.live_grep, { desc = "String in cwd" })
+		keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "String under cursor in cwd" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffer" })
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help page" })
+		keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "Man page" })
+		keymap.set("n", "<leader>fv", builtin.vim_options, { desc = "Vim option" })
+		keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymap" })
+		keymap.set("n", "<leader>fl", builtin.highlights, { desc = "Available highlights" })
+		keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Commits" })
+		keymap.set("n", "<leader>gC", builtin.git_bcommits, { desc = "Buffer's commits" })
+		keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Branches" })
+		keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Status" })
+		keymap.set("n", "<leader>gS", builtin.git_stash, { desc = "Stash" })
 	end,
 }
