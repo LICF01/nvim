2efbb2aeeb0e1917089563a06921678515674529
LICF01 <lucascubilla@gmail.com>
2023-10-13 09:14:34 -0300

    add formatters and linters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -2,37 +2,50 @@ return {
 	"stevearc/conform.nvim",
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cF",
+			function()
+				require("conform").format({ formatters = { "injected" } })
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({
+					timeout_ms = 500,
+					lsp_fallback = true,
+				})
+			end,
+			mode = { "n", "v" },
+			desc = "Format file or range (in visual mode)",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			["javascript"] = { { "prettierd", "prettier" } },
+			["javascriptreact"] = { { "prettierd", "prettier" } },
+			["typescript"] = { { "prettierd", "prettier" } },
+			["typescriptreact"] = { { "prettierd", "prettier" } },
+			["vue"] = { { "prettierd", "prettier" } },
+			["css"] = { { "prettierd", "prettier" } },
+			["scss"] = { { "prettierd", "prettier" } },
+			["less"] = { { "prettierd", "prettier" } },
+			["html"] = { { "prettierd", "prettier" } },
+			["json"] = { { "prettierd", "prettier" } },
+			["jsonc"] = { { "prettierd", "prettier" } },
+			["yaml"] = { { "prettierd", "prettier" } },
+			["markdown"] = { { "prettierd", "prettier" } },
+			["markdown.mdx"] = { { "prettierd", "prettier" } },
+			["graphql"] = { { "prettierd", "prettier" } },
+			["handlebars"] = { { "prettierd", "prettier" } },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,16 +1,16 @@
 return {
 	"mfussenegger/nvim-lint",
 	lazy = true,
-	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+	events = { "BufWritePost", "BufReadPost", "InsertLeave" },
 	config = function()
 		local lint = require("lint")
 
-		-- lint.linters_by_ft = {
-		-- 	javascript = { "eslint_d" },
-		-- 	typescript = { "eslint_d" },
-		-- 	javascriptreact = { "eslint_d" },
-		-- 	typescriptreact = { "eslint_d" },
-		-- }
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
