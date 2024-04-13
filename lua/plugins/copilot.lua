c702388a1c9659e64e6c1d136bf38ee39ccf2c4e
LICF01 <lucascubilla@gmail.com>
2024-04-13 00:38:38 -0400

    Update copilot keybindings, update lazy-lock

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -27,40 +27,42 @@ return {
 			language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
 			-- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
 			-- temperature = 0.1,
+			window = {
+				layout = "float",
+			},
 		},
 		build = function()
-			vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+			vim.notify(
+				"Please update the remote plugins by running ':UpdateRemotePlugins', then restart NeovimCopilotChatToggleCopilotChatToggleCopilotChatToggle."
+			)
 		end,
 		event = "VeryLazy",
 		keys = {
-			{ "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-			{ "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+			{ "<leader>tcc", "<cmd>CopilotChatOpen<cr>", desc = "Open chat" },
+			{ "<leader>tct", "<cmd>CopilotChatToggle<cr>", desc = "Toggle chat" },
+			{ "<leader>tcr", "<cmd>CopilotChatReset<cr>", desc = "Reset chat history and clear buffer" },
+			{ "<leader>tcf", "<cmd>CopilotChatFix<cr>", desc = "Find fix for error" },
+			{ "<leader>tcF", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "Assit with diagnostic" },
+			{ "<leader>tce", "<cmd>CopilotChatExplain<cr>", desc = "Explain" },
+			{ "<leader>tcE", "<cmd>CopilotChatTests<cr>", desc = "Explain & generate Tests" },
+			{ "<leader>tco", "<cmd>CopilotChatOptimize<cr>", desc = "Optimize selected code" },
+			{ "<leader>tcd", "<cmd>CopilotChatDocs<cr>", desc = "Write documentation for the selected code" },
+			{ "<leader>tcw", "<cmd>CopilotChatCommit<cr>", desc = "Write commit with commitizen convention" },
 			{
-				"<leader>ccT",
-				"<cmd>CopilotChatVsplitToggle<cr>",
-				desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
+				"<leader>tch",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+				end,
+				desc = "Show Help actions",
 			},
 			{
-				"<leader>ccv",
-				":CopilotChatVisual",
-				mode = "x",
-				desc = "CopilotChat - Open in vertical split",
-			},
-			{
-				"<leader>ccx",
-				":CopilotChatInPlace<cr>",
-				mode = "x",
-				desc = "CopilotChat - Run in-place code",
-			},
-			{
-				"<leader>ccf",
-				"<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
-				desc = "CopilotChat - Fix diagnostic",
-			},
-			{
-				"<leader>ccr",
-				"<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
-				desc = "CopilotChat - Reset chat history and clear buffer",
+				"<leader>tcp",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+				end,
+				desc = "Show Prompt actions",
 			},
 		},
 	},
