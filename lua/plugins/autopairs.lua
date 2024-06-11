98e24e7d1fdc402aa7bb01e6ababfef91836133f
LICF01 <lucascubilla@gmail.com>
2024-06-10 23:29:38 -0400

    Add nvim-autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,12 @@
+return {
+
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = true,
+	opts = {
+		check_ts = true,
+		ts_config = {
+			javascript = { "template_string", "string" },
+		},
+	},
+}
