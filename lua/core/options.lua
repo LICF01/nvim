12c8e74dab0d5c539ab8395d493c6cd867af57af
lucas <lucascubilla@gmail.com>
2025-04-26 15:34:10 -0300

    Update opts

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,11 +7,12 @@ opt.termguicolors = true -- true color support
 opt.cursorline = true -- highlight the current line
 opt.number = true -- enable line numbers
 opt.relativenumber = true -- Relative line numbers
-opt.scrolloff = 4 -- lines of context
-opt.sidescrolloff = 4 -- columns of context
+opt.scrolloff = 10 -- lines of context
+opt.sidescrolloff = 10 -- columns of context
 opt.wrap = false -- disable line wrapping
 opt.splitbelow = true -- split below instead of above
 opt.splitright = true -- split right instead of left
+opt.cmdheight = 0 -- Hides the command line when not in use
 
 -- Indentation
 opt.expandtab = false -- convert tabs to spaces
@@ -20,6 +21,7 @@ opt.shiftwidth = 2 -- change the number of space characters inserted for indenta
 opt.smartindent = true -- do smart autoindenting when starting a new line
 opt.autoindent = true -- copy the indent of the previous line
 opt.smarttab = true -- make tab behaviour smarter
+opt.breakindent = true -- wrap long lines visually
 
 -- Clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
