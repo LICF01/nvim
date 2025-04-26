d5338f535f780540f29d1f3ccab7bf3fcfca25c3
lucas <lucascubilla@gmail.com>
2025-04-26 15:32:14 -0300

    Update obsidian and markdown setup

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -15,6 +15,7 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	opts = {
+		ui = { enable = false },
 		workspaces = {
 			{
 				name = "personal",
@@ -24,6 +25,32 @@ return {
 				name = "work",
 				path = "~/Documents/notes/work",
 			},
+		},
+		note_id_func = function(title)
+			return title
+		end,
+	},
+	mappings = {
+		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		["gf"] = {
+			action = function()
+				return require("obsidian").util.gf_passthrough()
+			end,
+			opts = { noremap = false, expr = true, buffer = true },
+		},
+		-- Toggle check-boxes.
+		["<leader>oc"] = {
+			action = function()
+				return require("obsidian").util.toggle_checkbox()
+			end,
+			opts = { buffer = true },
+		},
+		-- Smart action depending on context, either follow link or toggle checkbox.
+		["<cr>"] = {
+			action = function()
+				return require("obsidian").util.smart_action()
+			end,
+			opts = { buffer = true, expr = true },
 		},
 	},
 }
