720cdafb68f3a1a4ef4125be8a36e3628b3c7155
lucas <lucascubilla@gmail.com>
2026-05-10 22:28:02 -0300

    Replace nvim-autopairs with mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,27 @@
 return {
-	{ "echasnovski/mini.icons", version = false },
+	{
+		"echasnovski/mini.icons",
+		version = false,
+		event = "VeryLazy",
+	},
 	{
 		"echasnovski/mini.pairs",
-		version = false,
-		config = function()
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+		config = function(_, opts)
 			local pairs = require("mini.pairs")
-			pairs.setup()
+			pairs.setup(opts)
 		end,
 	},
 	{
