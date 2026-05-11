f4d4f1cfcfe2b6dadf591abe21c7d282ef7f54e8
lucas <lucascubilla@gmail.com>
2026-05-11 11:29:21 -0300

    Update mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,11 @@
 return {
 	{
-		"echasnovski/mini.icons",
+		"nvim-mini/mini.icons",
 		version = false,
 		event = "VeryLazy",
 	},
 	{
-		"echasnovski/mini.pairs",
+		"nvim-mini/mini.pairs",
 		event = "VeryLazy",
 		opts = {
 			modes = { insert = true, command = true, terminal = false },
@@ -25,60 +25,62 @@ return {
 		end,
 	},
 	{
-		"echasnovski/mini.cursorword",
+		"nvim-mini/mini.cursorword",
+		event = "VeryLazy",
 		version = false,
 		config = function()
 			local cursorword = require("mini.cursorword")
 			cursorword.setup()
 		end,
 	},
-	--
-	-- {
-	-- 	"echasnovski/mini.completion",
-	-- 	version = false,
-	-- 	config = function()
-	-- 		local completion = require("mini.completion")
-	-- 		completion.setup()
-	-- 	end,
-	-- },
 	{
-		"echasnovski/mini.surround",
-		keys = function(_, keys)
-			-- Populate the keys based on the user's options
-			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-			local mappings = {
-				{ opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-				{ opts.mappings.delete, desc = "Delete surrounding", mode = { "n", "v" } },
-				{ opts.mappings.find, desc = "Find right surrounding", mode = { "n", "v" } },
-				{ opts.mappings.find_left, desc = "Find left surrounding", mode = { "n", "v" } },
-				{ opts.mappings.highlight, desc = "Highlight surrounding", mode = { "n", "v" } },
-				{ opts.mappings.replace, desc = "Replace surrounding", mode = { "n", "v" } },
-				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`", mode = { "n", "v" } },
-			}
-			mappings = vim.tbl_filter(function(m)
-				return m[1] and #m[1] > 0
-			end, mappings)
-			return vim.list_extend(mappings, keys)
-		end,
+		"nvim-mini/mini.surround",
+		event = "VeryLazy",
 		opts = {
-			n_lines = 40,
+			n_lines = 20,
 			mappings = {
-				add = "<leader>za", -- Add surrounding in Normal and Visual modes
-				delete = "<leader>zd", -- Delete surrounding
-				find = "<leader>zf", -- Find surrounding (to the right)
-				find_left = "<leader>zF", -- Find surrounding (to the left)
-				highlight = "<leader>zh", -- Highlight surrounding
-				replace = "<leader>zr", -- Replace surrounding
-				update_n_lines = "<leader>zn", -- Update `n_lines`
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
 			},
 		},
 	},
 	{
-		"echasnovski/mini.ai",
-		config = function()
+		"nvim-mini/mini.ai",
+		event = "VeryLazy",
+		opts = function()
 			local ai = require("mini.ai")
-			ai.setup()
+			return {
+				n_lines = 500,
+				custom_textobjects = {
+					o = ai.gen_spec.treesitter({ -- code block
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}),
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+					d = { "%f[%d]%d+" }, -- digits
+					e = { -- Word with case
+						{
+							"%u[%l%d]+%f[^%l%d]",
+							"%f[%S][%l%d]+%f[^%l%d]",
+							"%f[%P][%l%d]+%f[^%l%d]",
+							"^[%l%d]+%f[^%l%d]",
+						},
+						"^().*()$",
+					},
+					u = ai.gen_spec.function_call(), -- u for "Usage"
+					U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("mini.ai").setup(opts)
 		end,
 	},
 }
