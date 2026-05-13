998d8a568388c69bda313ac2abcc6372c02b44fa
lucas <lucascubilla@gmail.com>
2026-05-13 00:05:27 -0300

    Add custom layout

--- lua/plugins/tiny-code-action.lua
+++ lua/plugins/tiny-code-action.lua
@@ -2,17 +2,40 @@ return {
 	"rachartier/tiny-code-action.nvim",
 	event = "LspAttach",
 	opts = {
-		backend = "vim",
+		--- The backend to use, currently only "vim", "delta", "difftastic", "diffsofancy" are supported
+		backend = "delta",
 		picker = {
 			"snacks",
-			opts = {},
+			opts = {
+				-- layout = "vertical",
+				layout = {
+					layout = {
+						backdrop = false,
+						width = 0.5,
+						min_width = 80,
+						height = 0.8,
+						min_height = 30,
+						box = "vertical",
+						border = true,
+						title = "{title} {live} {flags}",
+						title_pos = "center",
+						{ win = "input", height = 1, border = "bottom" },
+						{ win = "list", border = "none" },
+						{ win = "preview", title = "{preview}", height = 0.6, border = "top" },
+					},
+				},
+			},
 		},
 	},
-
-  -- stylua: ignore
 	keys = {
-		{ "<leader>ca", function() require("tiny-code-action").code_action() end, desc = "Actions", },
-	}
-
-,
+		{
+			"<leader>ca",
+			function()
+				require("tiny-code-action").code_action()
+			end,
+			mode = { "n", "x" },
+			desc = "Actions",
+			noremap = true,
+		},
+	},
 }
